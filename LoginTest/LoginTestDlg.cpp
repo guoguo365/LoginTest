876f49ea8b2f51daecf1459d645bda4499ec2c5b
Administrator <Administrator@XFZ-20230419EDO>
2023-06-09 10:03:38 +0800

    Add project files.

---
+++ LoginTest/LoginTestDlg.cpp
@@ -0,0 +1,207 @@
+﻿
+// LoginTestDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "LoginTest.h"
+#include "LoginTestDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CLoginTestDlg 对话框
+
+
+
+CLoginTestDlg::CLoginTestDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_LOGINTEST_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CLoginTestDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT_USERNAME, m_edit_username);
+	DDX_Control(pDX, IDC_EDIT_USERNAME2, m_edit_password);
+}
+
+BEGIN_MESSAGE_MAP(CLoginTestDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDCANCEL, &CLoginTestDlg::OnBnClickedCancel)
+	ON_BN_CLICKED(IDOK, &CLoginTestDlg::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CLoginTestDlg 消息处理程序
+
+BOOL CLoginTestDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CLoginTestDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CLoginTestDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CLoginTestDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CLoginTestDlg::OnBnClickedCancel()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnCancel();
+}
+
+
+void CLoginTestDlg::OnBnClickedOk()
+{
+	// 获取账号
+	CString str_username;
+	str_username.GetBufferSetLength(20);
+	m_edit_username.GetWindowTextW(str_username.GetBuffer(), str_username.GetLength());
+	str_username.Trim();
+	int a = str_username.GetLength();
+	if (str_username == "") 
+	{
+		MessageBoxW(L"用户名不能为空", MB_OK);
+		return;
+	}
+
+	// 获取密码
+	CString str_password;
+	str_password.GetBufferSetLength(20);
+	m_edit_password.GetWindowTextW(str_password.GetBuffer(), str_password.GetLength());
+	if (str_password == "") 
+	{
+		MessageBoxW(L"密码不能为空", MB_OK);
+		return;
+	}
+
+	if (str_username != "admin" || str_password != "123456")
+	{
+		MessageBoxW(L"用户名或密码错误", MB_OK);
+		return;
+	}
+	MessageBoxW(L"登录成功", MB_OK);
+
+	str_username.ReleaseBuffer();
+	str_password.ReleaseBuffer();
+	
+	system("pause");
+
+
+	CDialogEx::OnOK();
+}
---
+++ LoginTest/LoginTestDlg.h
@@ -0,0 +1,39 @@
+﻿
+// LoginTestDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CLoginTestDlg 对话框
+class CLoginTestDlg : public CDialogEx
+{
+// 构造
+public:
+	CLoginTestDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LOGINTEST_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CEdit m_edit_username;
+	CEdit m_edit_password;
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnBnClickedOk();
+};
